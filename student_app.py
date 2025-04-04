5ec0ffe912ab199de3e7551ac61edb841b85b60e
Jenny Ramsey <jenniferramsey@hotmail.co.uk>
2025-04-04 12:57:14 +0100

    Notes on differences between dictionaries and classes

--- student_app.py
+++ student_app.py
@@ -1,3 +1,6 @@
+# use an import to carry over classes from other files. The file in this case is called toy and we are 
+# importing the class Toy
+
 from toy import Toy
 
 toys = [
@@ -13,8 +16,33 @@ toys = [
 
 # Make a toys list with at least 2 dictionaries representing a toy with name and colour DONE
 
-# create a function to print out the name of all toys
+# Task- create a function to print out the name of all toys
 
-# create a function to print out all colours of toys. print(print_colours()) => ['red', 'green']
+# notes on the process:
+# 1. create the function first 
+# 2. create a for loop to iterate through the toys
+# 3. print just the name of the toys- use square brackets to iterate through
+# 4. call the function again
 
-# Now do this with classes
+def print_names():
+    for toy in toys:
+        print(toy["name"])
+print_names()
+
+# Task - create a function to print out all colours of toys. print(print_colours()) => ['red', 'green']
+# notes - this prints the function out in the print instead of calling it
+
+def print_colours():
+    return [toy ["colour"] for toy in toys]
+print(print_colours())
+
+
+# take it further by storing it in a variable
+
+colours = print_colours()
+print(colours)
+
+        
+# Now do this with classes- see toy.py
+
+
--- toy.py
+++ toy.py
@@ -1,2 +1,25 @@
+# 1. Create the class so that it is a blue print. Always with def__init__ and self
+# 2. List the toy options
+# 3. Create the functions for print names and print colours
+# 4. Classes use functions and lists instead of dictionaries 
+
 class Toy:
-    pass
+    def __init__ (self, name, colour):
+        self.name = name
+        self.colour = colour
+
+toys = [
+    Toy("Ball", "red"),
+    Toy("Teddy", "brown")
+    ]
+
+def print_names():
+    for toy in toys:
+        print(toy.name)
+
+def print_colours():
+        return [toy.colour for toy in toys]
+
+print_names()
+print(print_colours())
+
